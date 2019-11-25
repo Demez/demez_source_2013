d60739cb944805b70c8888b7f7b8db7060d985f0
Demez <demizexp@gmail.com>
2019-11-24 23:43:34 -0500

    [QPC] some things i missed

--- src/_create_game_projects.bat
+++
@@ -1,4 +0,0 @@
-
-py devtools/qpc/qpc.py /basefile "_qpc_scripts/_default.qpc_base" /add everything /macros DEMEZ_HL2 /types vstudio /masterfile games
-
-pause
---
+++ src/_create_gamedlls.bat
@@ -0,0 +1,4 @@
+
+py devtools/qpc/qpc.py /basefile "_qpc_scripts/_default.qpc_base" /add gamedlls /macros DEMEZ_HL2 /types vstudio /masterfile gamedlls
+
+pause
